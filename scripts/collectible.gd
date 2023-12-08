db7c6542f17f2eb75ca43358eff2f8ba228bf716
Kevin Wu <contact@kevinwu.io>
2023-12-08 13:08:55 -0800

    Removed tutorial project

--- scripts/collectible.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody2D
-
-@export var value = 10
-
-func Collect():
-	queue_free()
--- scripts/level.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-@onready var ui = %UI
-
-func update_ui_score(value):
-	ui.update_score(value)
